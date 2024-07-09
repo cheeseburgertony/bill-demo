7462246292e42e653e6f35c27214fda107a5c4d5
cheeseburgertony <1821955020@qq.com>
2024-07-09 18:18:06 +0800

    feat:tabBar切换功能

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,24 +1,57 @@
-import { getBillList } from "@/store/modules/billStore"
-import { Button } from "antd-mobile"
+import { TabBar } from "antd-mobile"
 import { useEffect } from "react"
-import { useDispatch } from "react-redux"
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigate } from "react-router-dom"
+import { useDispatch } from 'react-redux'
+import { getBillList } from "@/store/modules/billStore"
+import './index.scss'
+import {
+  BillOutline,
+  CalculatorOutline,
+  AddCircleOutline
+} from 'antd-mobile-icons'
+
+const tabs = [
+  {
+    key: '/',
+    title: '月度账单',
+    icon: <BillOutline />,
+  },
+  {
+    key: '/new',
+    title: '记账',
+    icon: <AddCircleOutline />,
+  },
+  {
+    key: '/year',
+    title: '年度账单',
+    icon: <CalculatorOutline />,
+  },
+]
 
 const Layout = () => {
-  // 渲染时触发方法获取数据
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(getBillList())
   }, [dispatch])
 
+  // 切换tab菜单 路由跳转
+  const navigate = useNavigate()
+  const switchRoute = (path) => {
+    navigate(path)
+  }
+
+
   return (
-    <div>
-      <Outlet />
-      我是Layout
-      {/* 测试全局生效样式 */}
-      <Button color="primary">测试全局样式</Button>
-      <div className="puple">
-        <Button color="primary">测试局部样式</Button>
+    <div className="layout">
+      <div className="container">
+        <Outlet />
+      </div>
+      <div className="footer">
+        <TabBar onChange={switchRoute}>
+          {tabs.map(item => (
+            <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+          ))}
+        </TabBar>
       </div>
     </div>
   )
